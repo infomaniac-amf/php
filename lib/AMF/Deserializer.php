89071de822c5f997279608124dc7f82f042e0b5c
Danny Kopping <dannykopping@gmail.com>
2014-02-23 23:18:38 +0200

    More tests!

--- lib/AMF/Deserializer.php
+++ lib/AMF/Deserializer.php
@@ -1,23 +1,136 @@
 <?php
 namespace Infomaniac\AMF;
 
+use DateTime;
+use Infomaniac\Exception\DeserializationException;
+use Infomaniac\IO\Input;
+use Infomaniac\Util\ReferenceStore;
+
 /**
  * @author Danny Kopping <dannykopping@gmail.com>
  */
-class Deserializer
+class Deserializer extends Base
 {
-    public static function deserialize($data)
+    /**
+     * @var \Infomaniac\IO\Input
+     */
+    protected $stream;
+
+    public function __construct(Input $stream)
     {
-        return self::deserializeUndefined($data);
+        parent::__construct($stream);
     }
 
-    private static function deserializeUndefined($data)
+    public function deserialize()
     {
-        return self::readBytes($data);
+        $type = $this->stream->readByte();
+
+        switch ($type) {
+            case Spec::AMF3_UNDEFINED:
+            case Spec::AMF3_NULL:
+            case Spec::AMF3_FALSE:
+            case Spec::AMF3_TRUE:
+                // the data type is the value for these simple types
+                return $type;
+                break;
+            case Spec::AMF3_INT:
+                return $this->deserializeInt();
+                break;
+            case Spec::AMF3_DOUBLE:
+                return $this->deserializeDouble();
+                break;
+            case Spec::AMF3_STRING:
+                return $this->deserializeString();
+                break;
+            case Spec::AMF3_DATE:
+                return $this->deserializeDate();
+                break;
+            default:
+                die("oh noes!");
+                break;
+        }
     }
 
-    private static function readBytes($data, $offset = 0, $length = 1)
+    private function deserializeInt()
     {
-        return ord(substr($data, $offset, $length));
+        $count = 1;
+        $int   = 0;
+
+        $byte = $this->stream->readByte();
+
+        while ((($byte & 0x80) != 0) && $count < 4) {
+            $int <<= 7;
+            $int |= ($byte & 0x7F);
+            $byte = $this->stream->readByte();
+            $count++;
+        }
+
+        if ($count < 4) {
+            $int <<= 7;
+            $int |= $byte;
+        } else {
+            // Use all 8 bits from the 4th byte
+            $int <<= 8;
+            $int |= $byte;
+        }
+
+        if (($int & 0x18000000) == 0x18000000) {
+            $int ^= 0x1fffffff;
+            $int *= -1;
+            $int -= 1;
+        } else {
+            if (($int & 0x10000000) == 0x10000000) {
+                // remove the signed flag
+                $int &= 0x0fffffff;
+            }
+        }
+
+        return $int;
+    }
+
+    private function deserializeDouble()
+    {
+        $double = $this->stream->readRawBytes(8, true);
+        if (Spec::isBigEndian()) {
+            $double = strrev($double);
+        }
+
+        $double = unpack('d', $double);
+        return array_pop($double);
+    }
+
+    private function deserializeString()
+    {
+        $reference = $this->deserializeInt();
+
+        if ($reference & Spec::REFERENCE_BIT == 0) {
+            $reference >>= Spec::REFERENCE_BIT;
+
+            return $this->referenceStore->getByReference($reference, ReferenceStore::TYPE_STRING);
+        }
+
+        $length = $reference << Spec::REFERENCE_BIT | 1;
+        $string = $this->stream->readRawBytes($length);
+        $this->referenceStore->addReference($string, ReferenceStore::TYPE_STRING);
+
+        return $string;
+    }
+
+    private function deserializeDate()
+    {
+        $reference = $this->deserializeInt();
+
+        if ($reference & Spec::REFERENCE_BIT == 0) {
+            $reference >>= Spec::REFERENCE_BIT;
+
+            return $this->referenceStore->getByReference($reference, ReferenceStore::TYPE_OBJECT);
+        }
+
+        $timestamp = $this->stream->readDouble() / 1000;
+        $date = new DateTime("@$timestamp");
+
+        $this->referenceStore->addReference($date, ReferenceStore::TYPE_OBJECT);
+
+        return $date;
     }
 } 
--- tests/SerializationTest.php
+++ tests/SerializationTest.php
@@ -14,53 +14,68 @@ class SerializationTest extends PHPUnit_Framework_TestCase
 {
     public function testSerializeUndefined()
     {
-        $this->assertEquals(Spec::AMF3_UNDEFINED, bin2hex(AMF::serialize(new Undefined())));
+        $this->assertSame(Spec::AMF3_UNDEFINED, AMF::deserialize(AMF::serialize(new Undefined())));
     }
 
     public function testSerializeNull()
     {
-        $this->assertEquals(Spec::AMF3_NULL, bin2hex(AMF::serialize(null)));
+        $this->assertSame(Spec::AMF3_NULL, AMF::deserialize(AMF::serialize(null)));
     }
 
     public function testSerializeBoolean()
     {
-        $this->assertEquals(Spec::AMF3_TRUE, bin2hex(AMF::serialize(true)));
-        $this->assertEquals(Spec::AMF3_FALSE, bin2hex(AMF::serialize(false)));
+        $this->assertSame(Spec::AMF3_FALSE, AMF::deserialize(AMF::serialize(false)));
+        $this->assertSame(Spec::AMF3_TRUE, AMF::deserialize(AMF::serialize(true)));
     }
 
     public function testSerializeInt()
     {
-        $samples      = [1, 13, 1398693, 100000000, 12345013, 9876543, Spec::getMaxInt() - 1];
-        $expectations = ["0401", "040d", "04d5af25", "0497ebe100", "0482f8deb5", "0482adb43f", "04bffffffe"];
+        $samples = [1, 13, 1398693, 100000000, 12345013, 9876543, Spec::getMaxInt() - 1, -123, -9999999];
 
         foreach ($samples as $sample) {
-            $expected = $expectations[array_search($sample, $samples)];
-
-            $this->assertEquals($expected, bin2hex(AMF::serialize($sample)));
+            $this->assertEquals($sample, AMF::deserialize(AMF::serialize($sample, Spec::AMF3_INT)));
         }
     }
 
     public function testSerializeDouble()
     {
-        $samples      = [1.5, 9879.4, 999 * 999 / 2, Spec::getMaxInt() + 2];
-        $expectations = ["053ff8000000000000", "0540c34bb333333333", "05411e74e200000000", "0541b0000001000000"];
+        $samples = [1.5, 9879.4, 999 * 999 / 2, Spec::getMaxInt() + 2.0, Spec::getMinInt() * 2];
 
         foreach ($samples as $sample) {
-            $expected = $expectations[array_search($sample, $samples)];
+            $this->assertEquals($sample, AMF::deserialize(AMF::serialize($sample, Spec::AMF3_DOUBLE)));
+        }
+    }
 
-            $this->assertEquals($expected, bin2hex(AMF::serialize($sample)));
+    public function testSerializeDate()
+    {
+        $samples = [
+            new DateTime(),
+            new DateTime('15 Jan 1989'),
+            new DateTime('31 December 1880'),
+            new DateTime('1 Jan 2079')
+        ];
+
+        foreach ($samples as $sample) {
+            $timestamp = $sample->format('U');
+            $datetime  = AMF::deserialize(AMF::serialize($sample, Spec::AMF3_DATE));
+
+            $this->assertEquals($timestamp, $datetime->format('U'));
         }
     }
 
     public function testSerializeString()
     {
-        $this->assertEquals('060b68656c6c6f', bin2hex(AMF::serialize('hello')));
+        $samples = ['hello', '.', file_get_contents(__FILE__), 'ünicødé'];
+
+        foreach ($samples as $sample) {
+            $this->assertSame($sample, AMF::deserialize(AMF::serialize($sample, Spec::AMF3_STRING)));
+        }
     }
 
     public function testSerializeObject()
     {
         // dynamic object
-        $dyn    = new stdClass();
+        $dyn = new stdClass();
         $dyn->a = 'b';
         $dyn->b = array('123');
 
